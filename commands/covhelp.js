33d86a001ba5056df4c69a8dda7bcfa370568d93
Kevin Pierce <kevinpierce08@gmail.com>
2020-07-27 15:48:31 -0400

    Implemented command-specific help embed for death cases

--- commands/covhelp.js
+++ commands/covhelp.js
@@ -18,5 +18,30 @@ module.exports = {
 
             return message.channel.send(helpMenuEmbed);
         }
+        else if (args.length == 1) {
+            if (args[0] === "deaths"){
+                const deathHelpEmbed = new Discord.MessageEmbed()
+                .setTitle("!deaths Help")
+                .setColor("#990000")
+                .addFields(
+                    { name: "```" + "!deaths" + "```", value: 'Returns total number of deaths globally' },
+                    { name: "```" + "!deaths [today/td]" + "```", value: 'Returns total number of deaths today globally' },
+                    { name: "```" + "!deaths [today/td] [country name]" + "```", value: 'Returns total number of deaths today in specified country'},
+                    { name: "```" + "!deaths [yesterday/ytd]" + "```", value: 'Returns total number of deaths yesterday globally' },
+                    { name: "```" + "!deaths [yesterday/ytd] [country name]" + "```", value: 'Returns total number of deaths yesterday in specified country'},
+                    { name: "```" + "!deaths [historic/hs]" + "```", value: 'Returns graph of historical death numbers for the past 30 days globally' },
+                    { name: "```" + "!deaths [historic/hs] [number of days]" + "```", value: 'Returns graph of historical death numbers for the past [number of days] globally'},
+                    { name: "```" + "!deaths [historic/hs] [number of days] [country name]" + "```", value: 'Returns graph of historical death numbers for the past [number of days] in specified country'}
+                    );
+
+                return message.channel.send(deathHelpEmbed);
+            }
+            else if (args[0] === "recovered"){
+                // do something else
+            }
+            else if (args[0] === "cases"){
+                // do something
+            }
+        }
     }
 }
